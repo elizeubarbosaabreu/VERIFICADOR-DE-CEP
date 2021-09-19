4aec96818718132b720076118d955a4012df66e3
Elizeu Barbosa Abreu <elizeubcorreios@gmail.com>
2021-09-18 23:47:39 -0400

    Update Valida_CEP_App.py

--- Valida_CEP_App.py
+++ Valida_CEP_App.py
@@ -12,17 +12,19 @@ import PySimpleGUI as sg
 
 sg.theme('Reddit')
 
-layout = [[sg.Image('Valida_CEP_API.png')],
+layout = [[sg.Text('VALIDA CEP APP', font=('Arial 20'), text_color='red')],
+          [sg.Text('''Este App verifica se um determinado CEP \u00e9 v\u00e1lido,
+retornando o nome do logradouro, bairro, cidade,
+UF, caso dispon\u00edveis...''', font=('Arial 12'), text_color='blue')],
           [sg.Text(
-              'Digite um CEP'), sg.Input(
+              'Digite um CEP', font=('Arial 12'), text_color='blue'), sg.Input(
                   key='-INPUT-', size=(15, 1)), sg.Button(
                       'Consultar')],
-          [sg.Image(
-              'imagem.png', size=(48, 48)), sg.Text(
-                  'https://github.com/elizeubarbosaabreu')]
+          [sg.Text(
+                  'Desenvolvido por @elizeu.barbosa.abreu', font=('Arial 12'), text_color='purple')]
           
     ]
-window = sg.Window('VALIDA CEP APP', layout, element_justification='c')
+window = sg.Window('VALIDA CEP APP', layout, resizable=True, element_justification='c')
 
 while True:
     event, values = window.read()    
@@ -32,8 +34,8 @@ while True:
     cep = values['-INPUT-']
             
     if len(cep) != 8:
-        sg.Popup('ERRO!!', '''Quantidade de dígitos inválida.
-Use apenas números sem pontos ou tracinhos. Ex.: 76916000''')
+        sg.Popup('ERRO!!', '''Quantidade de d\u00edgitos inv\u00e1lida.
+Use apenas n\u00fameros sem pontos ou tracinhos. Ex.: 76916000''', font=('Arial 12'), text_color='red')
         continue
         
     request = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
@@ -71,9 +73,10 @@ CIDADE: {localidade}
 ESTADO: {uf}
 
 fonte: https://viacep.com.br
-''')
+''', font=('Arial 12'), text_color='blue')
     else:
-        sg.Popup('CEP NÃO LOCALIZADO!!!', f'{cep} não localizado em nosso banco de dados!!!')
+        sg.Popup(
+            'CEP N\u00c3O LOCALIZADO!!!', f'{cep} n\u00e3o localizado em nosso banco de dados!!!', font=('Arial 12'), text_color='red')
     
 
 window.close()
