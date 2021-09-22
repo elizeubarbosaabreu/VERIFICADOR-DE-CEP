9712613b55406f43bd17115330f184e61c686d97
Elizeu Barbosa Abreu <elizeubcorreios@gmail.com>
2021-09-21 20:09:28 -0400

    Update Valida_CEP_App.py

--- Valida_CEP_App.py
+++ Valida_CEP_App.py
@@ -12,19 +12,19 @@ import PySimpleGUI as sg
 
 sg.theme('Reddit')
 
-layout = [[sg.Text('VALIDA CEP APP', font=('Arial 20'), text_color='red')],
-          [sg.Text('''Este App verifica se um determinado CEP \u00e9 v\u00e1lido,
+layout = [[sg.Stretch(), sg.Text('VALIDA CEP APP', font=('Arial 20'), text_color='red'), sg.Stretch()],
+          [sg.Text('''Este App verifica se um determinado CEP é válido,
 retornando o nome do logradouro, bairro, cidade,
-UF, caso dispon\u00edveis...''', font=('Arial 12'), text_color='blue')],
+UF, caso disponíveis...''', font=('Arial 12'), text_color='blue')],
           [sg.Text(
               'Digite um CEP', font=('Arial 12'), text_color='blue'), sg.Input(
-                  key='-INPUT-', size=(15, 1)), sg.Button(
+                  key='-INPUT-', size=(15, 1)), sg.Stretch(), sg.Button(
                       'Consultar')],
-          [sg.Text(
-                  'Desenvolvido por @elizeu.barbosa.abreu', font=('Arial 12'), text_color='purple')]
+          [sg.Stretch(), sg.Text(
+                  'Desenvolvido por @elizeu.barbosa.abreu', font=('Arial 10'), text_color='purple'), sg.Stretch()]
           
     ]
-window = sg.Window('VALIDA CEP APP', layout, resizable=True, element_justification='c')
+window = sg.Window('VALIDA CEP APP', layout, resizable=True)
 
 while True:
     event, values = window.read()    
@@ -34,8 +34,8 @@ while True:
     cep = values['-INPUT-']
             
     if len(cep) != 8:
-        sg.Popup('ERRO!!', '''Quantidade de d\u00edgitos inv\u00e1lida.
-Use apenas n\u00fameros sem pontos ou tracinhos. Ex.: 76916000''', font=('Arial 12'), text_color='red')
+        sg.Popup('ERRO!!', '''Quantidade de dígitos inválida ou caracteres inválidos.
+Use apenas números sem pontos ou tracinhos. Ex.: 76916000''', font=('Arial 12'), text_color='red')
         continue
         
     request = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
@@ -76,7 +76,8 @@ fonte: https://viacep.com.br
 ''', font=('Arial 12'), text_color='blue')
     else:
         sg.Popup(
-            'CEP N\u00c3O LOCALIZADO!!!', f'{cep} n\u00e3o localizado em nosso banco de dados!!!', font=('Arial 12'), text_color='red')
+            'CEP NÃO LOCALIZADO!!!', f'{cep} não localizado em nosso banco de dados!!!', font=('Arial 12'), text_color='red')
     
 
 window.close()
+
